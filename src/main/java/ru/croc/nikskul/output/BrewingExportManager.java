a007db39a4248a8c5f2a088a44caecfe7c9a147f
NSkulyberdin <nskulyberdin@rockitsoft.ru>
2023-05-11 16:09:37 +0300

    Add results and fix warns

--- src/main/java/ru/croc/nikskul/output/BrewingExportManager.java
+++ src/main/java/ru/croc/nikskul/output/BrewingExportManager.java
@@ -1,18 +1,12 @@
 package ru.croc.nikskul.output;
 
 import ru.croc.nikskul.domain.BrewingLog;
-import ru.croc.nikskul.domain.Tea;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -38,21 +32,19 @@ public class BrewingExportManager {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(destination.getPath()))) {
 
             var dates = brewingLogs.stream()
-                .map(brewing -> {
-                    return LocalDate.from(brewing.getBrewingStart());
-                })
+                .map(brewing -> LocalDate.from(brewing.getBrewingStart()))
                 .collect(Collectors.toSet());
 
             dates.forEach(date -> {
                 try {
-                    StringBuilder builder = new StringBuilder();
+
                     var dateBrew = brewings.stream().filter(brew ->
-                            LocalDate.from(brew.getBrewingStart())
-                                .compareTo(date) == 0)
+                            LocalDate.from(brew.getBrewingStart()).isEqual(date))
                         .collect(Collectors.toSet());
 
                     String employees = dateBrew.stream()
                         .map(BrewingLog::getEmployeeName)
+                        .distinct()
                         .collect(Collectors.joining(", "));
 
                     writer.write(date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy - ")) + employees);
